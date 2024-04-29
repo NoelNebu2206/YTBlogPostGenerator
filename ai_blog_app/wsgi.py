f5909dd7acfd4354d8401874d4f748ea1f60d330
NoelNebu2206 <nnp5666@nyu.edu>
2024-04-29 02:45:40 -0400

    updated wsgi.py file

--- ai_blog_app/wsgi.py
+++ ai_blog_app/wsgi.py
@@ -13,4 +13,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ai_blog_app.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+django_app = get_wsgi_application()
+
+
+def https_app(environ, start_response):
+    environ["wsgi.url_scheme"] = "https"
+    return django_app(environ, start_response)
+
+
+application = https_app
